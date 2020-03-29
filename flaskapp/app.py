7fda56f6b0d25cba134da8909ba6d80ace5aa6a3
Tom <timessquaregt@gmail.com>
2020-03-29 21:18:59 +0100

    changed run.py to app.py and added Movie database to hold recommendations.

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -9,13 +9,16 @@ db = SQLAlchemy(app)
 class Movie(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	imdb_id = db.Column(db.String, unique=True)
-	title = db.Column(db.String)
-	rec_1 = db.Column(db.String)
-	rec_2 = db.Column(db.String)
-	rec_3 = db.Column(db.String)
-	rec_4 = db.Column(db.String)
-	rec_5 = db.Column(db.String)
-	rec_6 = db.Column(db.String)
+	title = db.Column(db.String, nullable=False)
+	rec_1 = db.Column(db.String, nullable=False)
+	rec_2 = db.Column(db.String, nullable=False)
+	rec_3 = db.Column(db.String, nullable=False)
+	rec_4 = db.Column(db.String, nullable=False)
+	rec_5 = db.Column(db.String, nullable=False)
+	rec_6 = db.Column(db.String, nullable=False)
+
+	def __repr__(self):
+		return f"{self.id}, {self.title}, {self.rec_1}, {self.rec_2} ..."
 
 @app.route('/')
 def hello():
