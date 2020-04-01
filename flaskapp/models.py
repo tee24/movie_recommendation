e265833756f515c8ea38f0a988cf762a51d4da00
Tom <timessquaregt@gmail.com>
2020-04-01 20:55:00 +0100

    added in movie metadeta to final_rec.csv also updated the models and db files to reflect changes

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -1,15 +1,22 @@
-from flaskapp.app import db, app
+from flaskapp.app import db
 
 class Movie(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	imdb_id = db.Column(db.String, unique=True)
 	title = db.Column(db.String, nullable=False)
+	overview = db.Column(db.String)
+	date_released = db.Column(db.DateTime)
+	poster_path = db.Column(db.String)
+	runtime = db.Column(db.Integer)
+	vote_count = db.Column(db.Integer)
+	vote_avg = db.Column(db.DECIMAL)
 	rec_1 = db.Column(db.String, nullable=False)
 	rec_2 = db.Column(db.String, nullable=False)
 	rec_3 = db.Column(db.String, nullable=False)
 	rec_4 = db.Column(db.String, nullable=False)
 	rec_5 = db.Column(db.String, nullable=False)
 	rec_6 = db.Column(db.String, nullable=False)
+
 
 	def __repr__(self):
 		return f"{self.id}, {self.title}, {self.rec_1}, {self.rec_2} ..."
