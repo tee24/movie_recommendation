bca4c190b58301a1d3b591b7682d24edecf657b9
Tom <timessquaregt@gmail.com>
2020-04-05 18:23:44 +0100

    updated html

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -20,4 +20,14 @@ class Movie(db.Model):
 	def __repr__(self):
 		return f"{self.id}, {self.title}, {self.rec_1}, {self.rec_2} ..."
 
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(16), unique=True, nullable=False)
+	email = db.Column(db.String(100), unique=True, nullable=False)
+	password = db.Column(db.String(30), nullable=False)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
 movie_list = [m.title for m in Movie.query.all()]
