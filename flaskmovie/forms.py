9837266f648e711087e7f58d47456b0fa5acb319
Tom <timessquaregt@gmail.com>
2020-04-05 20:44:53 +0100

    added custom validators for email/username unique checks

--- flaskmovie/forms.py
+++ flaskmovie/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskmovie.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,14 @@ class RegistrationForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password',
 									 validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
+
+	def validate_username(self, username):
+		if User.query.filter_by(username=username.data).first():
+			raise ValidationError("Username is already taken.")
+
+	def validate_email(self, email):
+		if User.query.filter_by(email=email.data).first():
+			raise ValidationError("Email is already taken.")
 
 class LoginForm(FlaskForm):
 	email = StringField('Email',
